cf7ba7bedc932e606efe087b93ed886e13bfe6d4
gnramos <gnramos@users.noreply.github.com>
2023-06-29 16:56:20 -0300

    Update arvore_binaria.py

--- packages/arvores/arvore_binaria.py
+++ packages/arvores/arvore_binaria.py
@@ -1,82 +1,71 @@
 class Node:
-    def __init__(self, initData):
-        self.data = initData
-        self.dir = self.esq = None
-    def getData(self):
-        return self.data
-    
-    def get_dir(self):
-        return self.dir
-    
-    def get_esq(self):
-        return self.esq
-    
+    def __init__(self, data):
+        self.data = data
+        self.right = self.left = None
 
-class ArvoreBinaria:
+    def __str__(self):
+        if self.left == self.right == None:
+            left = right = ''
+        else:
+            left = str(self.left) if self.left else '()'
+            right = str(self.right) if self.right else '()'
+        return f'({self.data}{left}{right})'
+
+
+class BinarySearchTree:
     def __init__(self):
         self.root = None
-        self.travessia = []
 
-    
-    def __search__(self, item, raiz):
+    def __str__(self):
+        return str(self.root) if self.root else ''
 
-        if raiz:
-            if item == raiz.data:
+    def __contains__(self, data):
+        def contains(root, data):
+            if root is None:
+                return False
+            if root.data == data:
                 return True
-            if item > raiz.getData():
-                return self.__search__(item, raiz.dir)
+            if root.data < data:
+                return contains(root.right, data)
+            return contains(root.left, data)
+
+        return contains(self.root, data)
+
+    def add(self, data):
+        def insert(root, data):
+            if root is None:
+                return Node(data)
+            if root.data < data:
+                root.right = insert(root.right, data)
             else:
-                return self.__search__(item, raiz.esq)
-        return False
-    
-    def __insert__(self, raiz, no):
-        if not raiz:
-            return no
-        
-        if no.getData() > raiz.getData():
-            raiz.dir = self.__insert__(raiz.dir, no)
-        else:
-            raiz.esq = self.__insert__(raiz.esq, no)
-        
-        return raiz
-    
-    def __travessia_pos__(self, node):
-        if node:
-            self.__travessia_pos__(node.esq)
-            self.__travessia_pos__(node.dir)
-            self.travessia.append(node.data)
+                root.left = insert(root.left, data)
+            return root
 
-    def __travessia_in__(self, node):
-        if node:
-            self.__travessia_in__(node.esq)
-            self.travessia.append(node.data)
-            self.__travessia_in__(node.dir)
+        self.root = insert(self.root, data)
 
-    def __travessia_pre__(self, node):
-        if node:
-            self.travessia.append(node.data)
-            self.__travessia_pre__(node.esq)
-            self.__travessia_pre__(node.dir)
-    
-    def __imprimir_travessia__(self):
-        print(' '.join(str(f) for f in self.travessia))
-        self.travessia.clear()
+    def inorder(self, process=lambda x: print(x.data, end=' ')):
+        def inorder_traversal(root):
+            if root:
+                inorder_traversal(root.left)
+                process(root)
+                inorder_traversal(root.right)
 
-    def insert(self, item):
-        no = Node(item)
-        self.root = self.__insert__(self.root, no)
+        inorder_traversal(self.root)
 
-    def travessia_pos(self):
-        self.__travessia_pos__(self.root)
-        self.__imprimir_travessia__()
+    def postorder(self, process=lambda x: print(x.data, end=' ')):
+        def postorder_traversal(root):
+            if root:
+                postorder_traversal(root.left)
+                postorder_traversal(root.right)
+                process(root)
 
-    def travessia_in(self):
-        self.__travessia_in__(self.root)
-        self.__imprimir_travessia__()
+        postorder_traversal(self.root)
 
-    def travessia_pre(self):
-        self.__travessia_pre__(self.root)
-        self.__imprimir_travessia__()
+    def preorder(self, process=lambda x: print(x.data, end=' ')):
+        def preorder_traversal(root):
+            if root:
+                process(root)
+                preorder_traversal(root.left)
+                preorder_traversal(root.right)
 
-    def search(self, item):
-        return self.__search__(item, self.root)
+        preorder_traversal(self.root)
